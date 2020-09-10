56b7852ac20dacd11135e8d9b1100b1389631d73
Shane Bee <shanebolenback@me.com>
2020-09-10 00:28:07 -0700

    Base commit

---
+++ Assets/Enemy.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour {
+
+    [SerializeField] private GameObject _cloudParticlePrefab;
+
+    private void OnCollisionEnter2D(Collision2D collision) {
+        bool didHitBird = collision.collider.GetComponent<Bird>() != null;
+        
+        if (didHitBird) {
+            Die();
+            return;
+        }
+
+        Enemy enemy = collision.collider.GetComponent<Enemy>();
+        if (enemy != null) {
+            return;
+        }
+
+        if (collision.contacts[0].normal.y < -0.5) {
+            Die();
+        }
+    }
+
+    private void Die() {
+        Instantiate(_cloudParticlePrefab, transform.position, Quaternion.identity);
+        Destroy(gameObject);
+    }
+
+}
